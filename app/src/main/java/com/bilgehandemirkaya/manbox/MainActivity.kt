68046a78470df0f8bde670ddd9ce87992c80773c
Emirhan Kaya <emrhanky3@gmail.com>
2023-12-18 14:34:26 +0300

    binding v1

--- app/src/main/java/com/bilgehandemirkaya/manbox/MainActivity.kt
+++ app/src/main/java/com/bilgehandemirkaya/manbox/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.bilgehandemirkaya.manbox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bilgehandemirkaya.manbox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{
+            val intent = Intent(this@MainActivity, MenuScreen::class.java)
+            startActivity(intent)
+        }
+        
     }
 }
