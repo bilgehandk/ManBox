e83138ac28d5a69a5788e5d5b39f681a1f8aab8a
armankurt <armanyilmazkurt@gmail.com>
2023-12-23 15:23:02 +0300

    Merge remote-tracking branch 'origin/main'

--- app/src/main/java/com/bilgehandemirkaya/manbox/AccountActivity.kt
+++ app/src/main/java/com/bilgehandemirkaya/manbox/AccountActivity.kt
@@ -2,16 +2,63 @@ package com.bilgehandemirkaya.manbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.bilgehandemirkaya.manbox.databinding.ActivityMainBinding
+import android.view.LayoutInflater
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.Observer
+import com.bilgehandemirkaya.manbox.database.LoginDB.Login
+import com.bilgehandemirkaya.manbox.database.LoginDB.LoginViewModel
+import com.bilgehandemirkaya.manbox.databinding.ActivityAccountBinding
 
 class AccountActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityAccountBinding
+    private lateinit var loginViewModel: LoginViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityAccountBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.button14.setOnClickListener{
+            showUserInfoDialog()
+        }
+    }
+
+    private fun showUserInfoDialog() {
+        val currentUserLiveData = loginViewModel.getCurrentUser()
+
+        currentUserLiveData.observe(this, Observer { userList ->
+            if (userList.isNotEmpty()) {
+                val currentUser = userList[0]
+                showCustomDialog(currentUser)
+            } else {
+                Toast.makeText(this, "User information not available", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun showCustomDialog(currentUser: Login) {
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog, null)
+        val nameSurnameTextView = dialogView.findViewById<TextView>(R.id.nameSurnameTextView)
+        val heightTextView = dialogView.findViewById<TextView>(R.id.heightTextView)
+        val weightTextView = dialogView.findViewById<TextView>(R.id.weightTextView)
+        val ageTextView = dialogView.findViewById<TextView>(R.id.ageTextView)
+
+        nameSurnameTextView.text = "Name: ${currentUser.name_surname}"
+        heightTextView.text = "Height: ${currentUser.height} cm"
+        weightTextView.text = "Weight: ${currentUser.weight} kg"
+        ageTextView.text = "Age: ${currentUser.age} years"
+
+        val builder = AlertDialog.Builder(this)
+        builder.setView(dialogView)
+            .setTitle("User Information")
+            .setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+            }
+
+        val dialog = builder.create()
+        dialog.show()
     }
 }
