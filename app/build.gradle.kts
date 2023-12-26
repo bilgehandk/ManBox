ded724e07bd66cae5326795042a6a4b38f9fcb26
Bilgehan Demirkaya <b.demirkaya@ug.bilkent.edu.tr>
2023-12-27 00:10:52 +0300

    Auto stash before merge of "main" and "origin/main"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
 
 
     implementation("androidx.core:core-ktx:1.9.0")
+    implementation ("com.google.code.gson:gson:2.8.8")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -53,6 +54,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+
+
 
 
 
